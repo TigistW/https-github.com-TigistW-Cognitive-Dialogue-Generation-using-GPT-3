db51a6116bb1f1a7cd4754ea3fbf656578d57c38
TigistW <wondimnehtigist@gmail.com>
2024-05-22 18:53:29 +0300

    add function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import Optional
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# Define input data model
+class InputText(BaseModel):
+    text: str
 
 @app.get("/")
 async def root():
@@ -13,9 +15,11 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/chat/{message}")
-def chat_with_bot(message:str):
-    return {"your message": message}
+@app.post("/chat")
+def chat_with_bot(input_text: InputText):
+    return {"your message": input_text.text}
 
+if __name__ == "__main__":
+    import uvicorn
 
-    
+    uvicorn.run(app, host="127.0.0.1", port=8000)
