cc67e794b553341990a5fffd009fdf7525325a7b
TigistW <wondimnehtigist@gmail.com>
2024-05-22 18:44:27 +0300

    add function

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/chat/{message}")
+def chat_with_bot(message:str):
+    return {"your message": message}
+
+
+    
